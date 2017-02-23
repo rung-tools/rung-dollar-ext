c69624b5250c07cfecc470928227fa92096f1dc3
Marcelo Camargo <marcelocamargo@linuxmail.org>
2017-02-23 15:17:23 -0300

    Make example work!!!

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const { create, run } = require('rung-sdk');
-const { OneOf, Double } = require('rung-sdk/dist/types');
+const { OneOf, Money } = require('rung-sdk/dist/types');
 const Bluebird = require('bluebird');
 const agent = require('superagent');
 const promisifyAgent = require('superagent-promise');
+const { path } = require('ramda');
 
 const request = promisifyAgent(agent, Bluebird);
 
 function main(context, done) {
     const { currency, value } = context.params;
 
-    return request.get('http://api.fixer.io/latest')
-        .then(({ body }) => {
-            done(body);
-        });
+    return request.get('http://api.fixer.io/latest?base=USD')
+        .then(path(['body', 'rates', currency]))
+        .then(dollar => dollar <= value
+            ? `Dollar is lower or equal to ${value}`
+            : null)
+        .then(done)
+        .catch(() => done());
 }
 
 const params = {
@@ -22,8 +26,8 @@ const params = {
         default: 'BRL'
     },
     value: {
-        description: 'When lower than this, we\'ll tell you',
-        type: Double,
+        description: 'When lower or equal to this, we\'ll tell you',
+        type: Money,
         default: 3.0
     }
 };
@@ -32,4 +36,3 @@ const app = create(main, { params });
 app.run();
 
 module.exports = app;
-
